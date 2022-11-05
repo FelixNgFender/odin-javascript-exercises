2a254b48c5906bcca0247af4c2af3ed2b3550552
Felix Nguyen <ngthinh302@gmail.com>
2022-11-05 14:56:13 -0400

    Finish first 7 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (string === '') {
+        return '';
+    }
+    else {
+        return reverseString(string.substr(1)) + string.charAt(0);
+    }
+    /* TOP solution
+    return string.split('').reverse().join('');
+     */
 };
 
 // Do not edit below this line
