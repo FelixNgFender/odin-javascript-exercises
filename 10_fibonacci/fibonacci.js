12a5503c3ac2a5077b8def5b48f1c1fabfdb193a
Felix Nguyen <ngthinh302@gmail.com>
2022-11-09 22:42:30 -0500

    JS basic noodling

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (i) {
+  if (i < 0) {
+    return "OOPS";
+  } else if (i <= 2) {
+    return 1;
+  } else {
+    return fibonacci(i - 1) + fibonacci(i - 2);
+  }
 };
 
 // Do not edit below this line
